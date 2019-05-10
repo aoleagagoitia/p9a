32b35670b04e2c1286ec97d5cf1aa964fe66ac5a
aoleagagoitia <aoleagagoitia@fp.uoc.edu>
2019-05-10 06:31:55 +0200

    CSS del menu, cabecera, barra lateral

--- index.php
+++ index.php
@@ -1,84 +1,87 @@
 <!DOCTYPE HTML>
 <html lang="es">
-    <head>
-        <meta charset="utf-8" />
-        <title>Tienda de Camisetas</title>
-        <link rel="stylesheet" href="assets/css/styles.css" />
-    </head>
-    <body>
+<head>
+    <meta charset="utf-8"/>
+    <title>Tienda de Camisetas</title>
+    <link rel="stylesheet" href="assets/css/styles.css"/>
+</head>
+<body>
+<div id="container">
+
     <!-- CABECERA -->
-        <header id="header">
-            <div id="logo">
-                <img src="assets/img/camiseta.png" alt="Camiseta Logo" />
-                <a href="index.php">
-                   Tienda de camisetas
-                </a>
-            </div>
-        </header>
+    <header id="header">
+        <div id="logo">
+            <img src="assets/img/camiseta.png" alt="Camiseta Logo"/>
+            <a href="index.php">
+                Tienda de camisetas
+            </a>
+        </div>
+    </header>
+
     <!-- MENU -->
-        <nav id="menu">
-            <ul>
-                <li>
-                    <a href="#">Inicio</a>
-                </li>
-                <li>
-                    <a href="#">Categoría 1</a>
-                </li>
-                <li>
-                    <a href="#">Categoría 2</a>
-                </li>
-                <li>
-                    <a href="#">Categoría 3</a>
-                </li>
-                <li>
-                    <a href="#">Categoría 4</a>
-                </li>
-                <li>
-                    <a href="#">Categoría 5</a>
-                </li>
-            </ul>
-        </nav>
+    <nav id="menu">
+        <ul>
+            <li>
+                <a href="#">Inicio</a>
+            </li>
+            <li>
+                <a href="#">Categoría 1</a>
+            </li>
+            <li>
+                <a href="#">Categoría 2</a>
+            </li>
+            <li>
+                <a href="#">Categoría 3</a>
+            </li>
+            <li>
+                <a href="#">Categoría 4</a>
+            </li>
+            <li>
+                <a href="#">Categoría 5</a>
+            </li>
+        </ul>
+    </nav>
 
     <div id="content">
 
-    <!-- BARRA LATERAL -->
+        <!-- BARRA LATERAL -->
         <aside id="lateral">
 
             <div id="login" class="block_aside">
+                <h3>Entrar a la web</h3>
                 <form action="#" method="post">
                     <label for="email">Email</label>
-                    <input type="email" name="email" />
+                    <input type="email" name="email"/>
                     <label for="password">Contraseña</label>
-                    <input type="password" name="password" />
-                    <input type="submit" value="Enviar" />
+                    <input type="password" name="password"/>
+                    <input type="submit" value="Enviar"/>
                 </form>
-
-                <a href="#">Mis pedidos</a>
-                <a href="#">Gestionar pedidos</a>
-                <a href="#">Gestionar categorías</a>
+                <ul>
+                    <li><a href="#">Mis pedidos</a></li>
+                    <li><a href="#">Gestionar pedidos</a></li>
+                    <li><a href="#">Gestionar categorías</a></li>
+                </ul>
             </div>
-
 
         </aside>
 
-
-    <!-- CONTENIDO CENTRAL -->
+        <!-- CONTENIDO CENTRAL -->
         <div id="central">
             <div class="product">
-                <img src="assets/img/camiseta.png" alt="Camiseta GitHub" />
+                <img src="assets/img/camiseta.png" alt="Camiseta GitHub"/>
                 <h2>Camiseta GitHub</h2>
                 <p>30€</p>
                 <a href="">Comprar</a>
 
                 <div class="product">
-                    <img src="assets/img/camiseta.png" alt="Camiseta GitHub" />
+                    <img src="assets/img/camiseta.png" alt="Camiseta GitHub"/>
                     <h2>Camiseta GitHub</h2>
                     <p>30€</p>
                     <a href="">Comprar</a>
                 </div>
 
                 <div class="product">
-                    <img src="assets/img/camiseta.png" alt="Camiseta GitHub" />
+                    <img src="assets/img/camiseta.png" alt="Camiseta GitHub"/>
                     <h2>Camiseta GitHub</h2>
                     <p>30€</p>
                     <a href="">Comprar</a>
@@ -86,11 +89,11 @@
             </div>
         </div>
     </div>
+
     <!-- FOOTER -->
     <footer id="footer">
-        <p>Desarrollado por Alfredo WEB &copy; <?=date('Y')?></p>
+        <p>Desarrollado por Alfredo WEB &copy; <?= date('Y') ?></p>
     </footer>
-
-    </body>
-
+</div>
+</body>
 </html>
